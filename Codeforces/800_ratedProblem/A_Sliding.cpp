5710682d8ff99d2d818f555618a7807cf544d770
Sahil_Hossain <sahilhossain1429@gmail.com>
2024-12-18 19:50:12 +0600

    took 40 mints total- 20 m Understanding, 5 m debug, rest debug

--- Codeforces/800_ratedProblem/A_Sliding.cpp
+++ Codeforces/800_ratedProblem/A_Sliding.cpp
@@ -1,18 +1,18 @@
 /**
  *	author 	: SahilH
-**/
+ **/
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 // Types
 #define ui unsigned int
-#define ll long long 
+#define ll long long
 #define vi vector<int>
 #define vll vector<ll>
 #define vvi vector<vector<int>>
-#define vpi vector<pair<int,int>>
-#define pii pair<int,int>
+#define vpi vector<pair<int, int>>
+#define pii pair<int, int>
 
 // Utils
 #define endl "\n"
@@ -20,21 +20,36 @@ using namespace std;
 #define sz(x) (int)(x).size()
 #define all(x) (x).begin(), (x).end()
 #define rall(x) (x).rbegin(), (x).rend()
-#define test(T) for(int t = 1; t <= T; ++t)
+#define test(T) for (int t = 1; t <= T; ++t)
 #define fastIO cin.tie(0)->sync_with_stdio(0)
 
 void solve()
 {
-    
+    ll totalRow, totalCol, personRow, personCol;
+    cin >> totalRow >> totalCol >> personRow >> personCol;
+
+    ll totalPerson = totalRow * totalCol;
+    ll perPosition = (personRow - 1) * totalCol + personCol;
+
+    ll rowLeft = (totalRow- personRow);
+    ll person_left = totalPerson - perPosition;
+
+    person_left -= rowLeft;
+
+    ll ans = rowLeft * (1 + (abs(1 - totalCol)));
+
+    cout << ans+person_left << endl;
 }
 
-int32_t main() {
+int32_t main()
+{
     fastIO;
     bool Q = true;
-    int T = Q? (cin >> T, T) : 1;
-    test(T) {
+    int T = Q ? (cin >> T, T) : 1;
+    test(T)
+    {
         solve();
     }
-    
+
     return 0;
 }
